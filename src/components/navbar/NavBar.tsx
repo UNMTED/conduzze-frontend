072dfa46a14417f32a79d19552db4e4bebce0aaa
Jefferson Carvalho <jeffersoncarvalhodosreis@gmail.com>
2025-11-07 11:28:55 -0300

    style: ajuste no navbar

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -6,39 +6,40 @@ export const NavBar = () => {
     const linkAtivo = "bg-white text-conduzze-dark";
     return (
         <>
-            <div className=" flex justify-between pt-5 py-8  border-b border-white px-10">
-                <img
-                    src="https://ik.imagekit.io/yljuedpj1/Conduzz%C3%A9%20(2).png?updatedAt=1762468743907"
-                    alt="Logo Conduzzé"
-                    className="h-10 sm:h-12 md:h-10 m-2 w-auto object-contain"
-                />
-                <nav className="flex lg:text-xl xl:text-2xl justify-end gap-2 mt-4 mb-auto">
-                    <NavLink
-                        to="/home"
-                        className={({ isActive }) =>
-                            `${linkBase} ${isActive ? linkAtivo : ""}`
-                        }
-                    >
-                        Início
-                    </NavLink>
+            <div className="border-b border-white">
+                <div className=" flex justify-between pt-5 py-8 max-w-7xl mx-auto">
+                    <img
+                        src="https://ik.imagekit.io/yljuedpj1/Conduzz%C3%A9%20(2).png?updatedAt=1762468743907"
+                        alt="Logo Conduzzé"
+                        className="h-10 sm:h-12 md:h-10 m-2 w-auto object-contain"
+                    />
+                    <nav className="flex lg:text-xl xl:text-2xl justify-end gap-2 mt-4 mb-auto">
+                        <NavLink
+                            to="/home"
+                            className={({ isActive }) =>
+                                `${linkBase} ${isActive ? linkAtivo : ""}`
+                            }
+                        >
+                            Início
+                        </NavLink>
 
-                    <NavLink
-                        to="/corridas"
-                        className={({ isActive }) =>
-                            `${linkBase} ${isActive ? linkAtivo : ""}`
-                        }
-                    >
-                        Corridas
-                    </NavLink>
-                    <NavLink
-                        to="/motoristas"
-                        className={({ isActive }) =>
-                            `${linkBase} ${isActive ? linkAtivo : ""}`
-                        }
-                    >
-                        Motoristas
-                    </NavLink>
-                    {/* <NavLink
+                        <NavLink
+                            to="/corridas"
+                            className={({ isActive }) =>
+                                `${linkBase} ${isActive ? linkAtivo : ""}`
+                            }
+                        >
+                            Corridas
+                        </NavLink>
+                        <NavLink
+                            to="/motoristas"
+                            className={({ isActive }) =>
+                                `${linkBase} ${isActive ? linkAtivo : ""}`
+                            }
+                        >
+                            Motoristas
+                        </NavLink>
+                        {/* <NavLink
                         to="/usuarios"
                         className={({ isActive }) =>
                             `${linkBase} ${isActive ? linkAtivo : ""}`
@@ -54,7 +55,8 @@ export const NavBar = () => {
                     >
                         Cadastro
                     </NavLink> */}
-                </nav>
+                    </nav>
+                </div>
             </div>
         </>
     );
